fd887dac426e8c4ab5fecde0e16e44d619254808
Riku5A <57973556+Riku5A@users.noreply.github.com>
2019-12-30 21:28:10 +0900

    inagaki

---
+++ Unity ActionGame/Assets/StaffAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaffAttack : MonoBehaviour
+{
+    private GameObject damageUI;
+    private AudioSource sound01;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound01 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Enemy")
+        {
+            col.transform.GetComponent<TakeDamage>().Damage(col);
+            sound01.PlayOneShot(sound01.clip);
+        }
+        if(col.tag == "Object")
+        {
+            sound01.PlayOneShot(sound01.clip);
+        }
+    }
+}
